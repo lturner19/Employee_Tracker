e912cbc2fb1d25111d57fa37adaa53b65b089460
Leandra Turner <calliet0715@gmail.com>
2020-03-07 19:57:38 -0600

    Creation of dependencies and database connection

--- employees.js
+++ employees.js
@@ -1,1 +1,14 @@
+//Dependency setup
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//allows access to the mysql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"Calomal15!",
+    database: "employees_db"
+})
+
 
