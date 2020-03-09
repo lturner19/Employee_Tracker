bcd209f8784078f0c9423793f50c0e1d784c3b31
Orchid23 <calliet0715@gmail.com>
2020-03-09 16:52:10 -0500

    Deletion of names from role, not a requirement

--- employees_db.sql
+++ employees_db.sql
@@ -27,18 +27,6 @@ Manager_Id INT NULL,
 PRIMARY KEY (Id)
 );
 
-
--- Data taken from the homework gif 
-INSERT INTO department(name)
-VALUES("Sales"),("Engineering"),("Finance"),("Legal");
-
-INSERT INTO role (Title, Salary, Department_Id)
-VALUES ("Sales Lead", 150000.00, 1), ("Salesperson", 130000.00, 1), ("Lead Engineer", 200000.00, 2),("Software Engineer", 185000.00, 2), ("Accountant", 85000.00, 3), ("Legal Team Lead", 250000.00, 4), ("Lawyer", 210000.00, 4);
-
-INSERT INTO employee (First_Name, Last_Name, Role_Id, Manager_Id)
-VALUES("John", "Doe", 1, 3), ("Mike", "Chan", 2, 1), ("Ashely", "Rodriquez", 3, null), ("Kevin", "Tupik", 3, 4), ("Malia", "Brown", 5, null), ("Sarah", "Lourd", 6, null), ("Tom", "Allen", 7, 7), ("Christian", "Eckenrode", 3, 2);
-
-
 -- Joining all three tables together to render all employees and info about them for the View All Employees requirement
 SELECT employee.id, employee.First_Name, employee.Last_Name, role.title, department.name department, role.salary, concat(employee2.First_Name, " ", employee2.Last_Name) manager
  FROM employee 
@@ -47,9 +35,7 @@ SELECT employee.id, employee.First_Name, employee.Last_Name, role.title, departm
  left join department on role.Department_Id = department.id Order By employee.id;
 
 -- see employees by department 
- SELECT * FROM department
+ SELECT * FROM department;
 
 --  see employees by role
-SELECT First_Name, Last_Name, Title
-FROM employee
-LEFT JOIN role ON employee.Role_Id = role.id;
+SELECT * FROM role;
